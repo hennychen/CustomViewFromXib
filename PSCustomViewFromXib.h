1036a6d33c2ff51b807741ae8baeb18e5f1e48e2
511308538@qq.com <511308538@qq.com>
2017-08-18 11:29:53 +0800

    customview xib

---
+++ PSCustomViewFromXib.h
@@ -0,0 +1,16 @@
+//
+//  PSCustomViewFromXib.h
+//  CustomView
+//
+//  Created by Paul Solt on 4/28/14.
+//  Copyright (c) 2014 Paul Solt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSCustomViewFromXib : UIView
+
+@property (nonatomic, strong) PSCustomViewFromXib *customView;
+- (UINavigationController*)navigationController;
+- (UIViewController*)viewController;
+@end
---
+++ PSCustomViewFromXib.m
@@ -0,0 +1,69 @@
+//
+//  PSCustomViewFromXib.m
+//  CustomView
+//
+//  Created by Paul Solt on 4/28/14.
+//  Copyright (c) 2014 Paul Solt. All rights reserved.
+//
+
+#import "PSCustomViewFromXib.h"
+
+@implementation PSCustomViewFromXib
+
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {        
+            // 1. Load the .xib file .xib file must match classname
+        NSString *className = NSStringFromClass([self class]);
+        _customView = [[[NSBundle mainBundle] loadNibNamed:className owner:self options:nil] firstObject];
+        _customView.frame = self.bounds;
+        // 2. Set the bounds if not set by programmer (i.e. init called)
+        if(CGRectIsEmpty(frame)) {
+            self.bounds = _customView.bounds;
+        }
+        
+        // 3. Add as a subview
+        [self addSubview:_customView];
+        
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if(self) {
+        
+        // 1. Load .xib file
+        NSString *className = NSStringFromClass([self class]);
+        _customView = [[[NSBundle mainBundle] loadNibNamed:className owner:self options:nil] firstObject];
+        _customView.frame = self.bounds;
+        // 2. Add as a subview
+        [self addSubview:_customView];
+        
+    }
+    return self;
+}
+- (UIViewController*)viewController {
+    for (UIView* next = [self superview]; next; next = next.superview) {
+        UIResponder* nextResponder = [next nextResponder];
+        if ([nextResponder isKindOfClass:[UIViewController class]]) {
+            return (UIViewController*)nextResponder;
+        }
+    }
+    return nil;
+}
+
+//获取导航控制器
+- (UINavigationController*)navigationController {
+    for (UIView* next = [self superview]; next; next = next.superview) {
+        UIResponder* nextResponder = [next nextResponder];
+        if ([nextResponder isKindOfClass:[UINavigationController class]]) {
+            return (UINavigationController*)nextResponder;
+        }
+    }
+    return nil;
+}
+
+@end
